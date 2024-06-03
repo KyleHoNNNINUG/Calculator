2cc9f0dbb54ef5273f85eafaa22e04fdb2a75e3a
KyleHoNNNINUG <kyleho0619@gmail.com>
2024-06-03 11:04:10 +0800

    update

--- src/Test.java
+++ src/Test.java
@@ -2,20 +2,9 @@ import java.util.Scanner;
 
 public class Test {
     private static final Scanner sc = new Scanner(System.in);
-    private static Function input() {
-        System.out.print("Enter the function to take derivative of: ");
-        String funcStr = sc.nextLine();
-        return stringToFunction(funcStr);
-    }
-    private static Function stringToFunction(String str) {
-        if (str == null || str.equals("")) return Function.ZERO;
-
-        
-        return null;
-    }
+    
     public static void main(String[] args) {
-        Function test = new ExponentialFunction(1, null, "x");
-        Function.printDerivative(test, 2);
+        
     }
 }
 
